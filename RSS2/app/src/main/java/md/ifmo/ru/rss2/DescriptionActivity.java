51b3e4186dc7f32282edd6707981c43b05adcf91
21011996 <21011996@mail.ru>
2015-01-17 14:43:03 +0300

    Added support for Viewing News in the browser.

--- RSS2/app/src/main/java/md/ifmo/ru/rss2/DescriptionActivity.java
+++ RSS2/app/src/main/java/md/ifmo/ru/rss2/DescriptionActivity.java
@@ -3,7 +3,10 @@ package md.ifmo.ru.rss2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 /**
  * Created by Илья on 16.01.2015.
@@ -15,11 +18,33 @@ public class DescriptionActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_description);
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         setTitle(intent.getStringExtra(FeedActivity.TITLE_EXTRA));
-        WebView wv = ((WebView) findViewById(R.id.web_view));
+        final WebView wv = ((WebView) findViewById(R.id.web_view));
         wv.getSettings().setDefaultTextEncodingName("utf-8");
         wv.loadDataWithBaseURL(null,getIntent().getStringExtra(FeedActivity.DESCRIPTION_EXTRA), "text/html",
                 "en_US",null);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WebView browser = ((WebView) findViewById(R.id.webView2));
+                browser.setWebViewClient(new AppWebViewClient());
+                if (intent.getStringExtra(FeedActivity.LINK_EXTRA) != null) {
+                    browser.loadUrl(intent.getStringExtra(FeedActivity.LINK_EXTRA));
+                    browser.setVisibility(View.VISIBLE);
+                    wv.setVisibility(View.GONE);
+                    button.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    private class AppWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url){
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
